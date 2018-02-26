acf13d1c15f113d28a215769d33640ccecc073a3
Kyle O'Gradya <ogradyks@clarkson.edu>
2018-02-26 15:40:08 -0500

    update

--- app/src/main/java/kyle/peaktracker/ClaimPeakActivity.java
+++ app/src/main/java/kyle/peaktracker/ClaimPeakActivity.java
@@ -355,21 +355,6 @@ public class ClaimPeakActivity extends AppCompatActivity {
         return nopath;
     }
 
-    /**
-     * Get the value of the data column for this Uri. This is <span id="IL_AD2"
-     * class="IL_AD">useful</span> for MediaStore Uris, and other file-based
-     * ContentProviders.
-     *
-     * @param context
-     *            The context.
-     * @param uri
-     *            The Uri to query.
-     * @param selection
-     *            (Optional) Filter used in the query.
-     * @param selectionArgs
-     *            (Optional) Selection arguments used in the query.
-     * @return The value of the _data column, which is typically a file path.
-     */
     public static String getDataColumn(Context context, Uri uri,
                                        String selection, String[] selectionArgs) {
 
@@ -391,45 +376,24 @@ public class ClaimPeakActivity extends AppCompatActivity {
         return "no path";
     }
 
-    /**
-     * @param uri
-     *            The Uri to check.
-     * @return Whether the Uri authority is ExternalStorageProvider.
-     */
     public static boolean isExternalStorageDocument(Uri uri) {
         return "com.android.externalstorage.documents".equals(uri
                 .getAuthority());
     }
 
-    /**
-     * @param uri
-     *            The Uri to check.
-     * @return Whether the Uri authority is DownloadsProvider.
-     */
     public static boolean isDownloadsDocument(Uri uri) {
         return "com.android.providers.downloads.documents".equals(uri
                 .getAuthority());
     }
 
-    /**
-     * @param uri
-     *            The Uri to check.
-     * @return Whether the Uri authority is MediaProvider.
-     */
     public static boolean isMediaDocument(Uri uri) {
         return "com.android.providers.media.documents".equals(uri
                 .getAuthority());
     }
 
-    /**
-     * @param uri
-     *            The Uri to check.
-     * @return Whether the Uri authority is Google Photos.
-     */
     public static boolean isGooglePhotosUri(Uri uri) {
         return "com.google.android.apps.photos.content".equals(uri
                 .getAuthority());
     }
-
 
 }
