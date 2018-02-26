84aa1ff9b12ac1e7117f93bf7b9f5434aa0151a6
Kyle O'Gradya <ogradyks@clarkson.edu>
2018-02-26 15:28:03 -0500

    Sorting working, images being rotated correctly

--- app/src/main/java/kyle/peaktracker/ClaimPeakActivity.java
+++ app/src/main/java/kyle/peaktracker/ClaimPeakActivity.java
@@ -1,10 +1,14 @@
 package kyle.peaktracker;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.content.ContentUris;
+import android.content.Context;
 import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -12,6 +16,9 @@ import android.graphics.Matrix;
 import android.graphics.Typeface;
 import android.media.ExifInterface;
 import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.provider.Settings;
 import android.support.v7.app.ActionBar;
@@ -41,6 +48,7 @@ import com.facebook.share.widget.ShareButton;
 import com.facebook.share.widget.ShareDialog;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -178,26 +186,60 @@ public class ClaimPeakActivity extends AppCompatActivity {
         if(reqCode == 2){
             if(resultCode == RESULT_OK) {
                 try {
+
+                    if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                            != PackageManager.PERMISSION_GRANTED) {
+
+                        // Should we show an explanation?
+                        if (shouldShowRequestPermissionRationale(
+                                Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                            // Explain to the user why we need to read the contacts
+                        }
+
+                        requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1000);
+
+                        // MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE is an
+                        // app-defined int constant that should be quite unique
+
+                        return;
+                    }
+
+
+
+
                     final Uri imageUri = data.getData();
                     final InputStream imageStream = getContentResolver().openInputStream(imageUri);
                     selectedImage = BitmapFactory.decodeStream(imageStream);
                     int width = selectedImage.getWidth();
                     int height = selectedImage.getHeight();
 
+                    Log.d("IMAGE WIDTH", Integer.toString(width));
+                    Log.d("IMAGE HEIGHT", Integer.toString(height));
+                    Matrix matrix = new Matrix();
+
+                    File file= new File(getPath(getApplicationContext(), imageUri));
+                    int orientation = 0;
+                    ExifInterface exif = null;
                     try {
-                        ExifInterface exif = new ExifInterface(getPath(imageUri));
-                    }catch (IOException e){
+                        exif = new ExifInterface(file.getAbsolutePath());
+                        orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+                        Log.d("ORIENTATION", Integer.toString(orientation));
+
+                    } catch (IOException e) {
                         e.printStackTrace();
                     }
 
-                    Matrix matrix = new Matrix();
-
+                    if(orientation == 6){
+                        matrix.setRotate(90);
+                    }
 
                     Bitmap selectedImageScaled = getResizedBitmap(selectedImage, height / 5, width / 5, matrix);
                     //Bitmap selectedImageScaled = Bitmap.createScaledBitmap(selectedImage, width/10, height/10, true);
-                    Log.d("SELECTED SIZE", Integer.toString(selectedImage.getByteCount()));
-                    Log.d("SELECTED SCALED SIZE", Integer.toString(selectedImageScaled.getByteCount()));
+//                    Log.d("SELECTED SIZE", Integer.toString(selectedImage.getByteCount()));
+//                    Log.d("SELECTED SCALED SIZE", Integer.toString(selectedImageScaled.getByteCount()));
 
+
+                    //Log.d("ORIENTATION", "MESSAGE " + path);
                     fbShareButton.setEnabled(true);
 
                     imageSaved = getBitmapAsByteArray(selectedImageScaled);
@@ -229,11 +271,13 @@ public class ClaimPeakActivity extends AppCompatActivity {
     {
         int width = bm.getWidth();
         int height = bm.getHeight();
+//        Log.d("IMAGE WIDTH", Integer.toString(width));
+//        Log.d("IMAGE HEIGHT", Integer.toString(height));
         float scaleWidth = ((float) newWidth) / width;
         float scaleHeight = ((float) newHeight) / height;
         // create a matrix for the manipulation
 
-
+       // m.preScale(1.0f, -1.0f);
 
         // resize the bit map
         m.postScale(scaleWidth, scaleHeight);
@@ -242,20 +286,149 @@ public class ClaimPeakActivity extends AppCompatActivity {
         return resizedBitmap;
     }
 
-    private static int exifToDegrees(int exifOrientation) {
-        if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_90) { return 90; }
-        else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_180) {  return 180; }
-        else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_270) {  return 270; }
-        return 0;
+    public static String getPath(final Context context, final Uri uri) {
+
+        String nopath = "no path";
+        // check here to KITKAT or new version
+        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+
+        // DocumentProvider
+        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+
+            // ExternalStorageProvider
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/"
+                            + split[1];
+                }
+            }
+            // DownloadsProvider
+            else if (isDownloadsDocument(uri)) {
+
+                final String id = DocumentsContract.getDocumentId(uri);
+                final Uri contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"),
+                        Long.valueOf(id));
+
+                return getDataColumn(context, contentUri, null, null);
+            }
+            // MediaProvider
+            else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[] { split[1] };
+
+                return getDataColumn(context, contentUri, selection,
+                        selectionArgs);
+            }
+        }
+        // MediaStore (and general)
+        else if ("content".equalsIgnoreCase(uri.getScheme())) {
+
+            // Return the remote address
+            if (isGooglePhotosUri(uri))
+                return uri.getLastPathSegment();
+
+            return getDataColumn(context, uri, null, null);
+        }
+        // File
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+
+        return nopath;
     }
 
-    private String getPath(Uri uri) {
-        String[]  data = { MediaStore.Images.Media.DATA };
-        CursorLoader loader = new CursorLoader(getApplicationContext(), uri, data, null, null, null);
-        Cursor cursor = loader.loadInBackground();
-        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-        cursor.moveToFirst();
-        return cursor.getString(column_index);
+    /**
+     * Get the value of the data column for this Uri. This is <span id="IL_AD2"
+     * class="IL_AD">useful</span> for MediaStore Uris, and other file-based
+     * ContentProviders.
+     *
+     * @param context
+     *            The context.
+     * @param uri
+     *            The Uri to query.
+     * @param selection
+     *            (Optional) Filter used in the query.
+     * @param selectionArgs
+     *            (Optional) Selection arguments used in the query.
+     * @return The value of the _data column, which is typically a file path.
+     */
+    public static String getDataColumn(Context context, Uri uri,
+                                       String selection, String[] selectionArgs) {
+
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = { column };
+
+        try {
+            cursor = context.getContentResolver().query(uri, projection,
+                    selection, selectionArgs, null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return "no path";
+    }
+
+    /**
+     * @param uri
+     *            The Uri to check.
+     * @return Whether the Uri authority is ExternalStorageProvider.
+     */
+    public static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri
+                .getAuthority());
+    }
+
+    /**
+     * @param uri
+     *            The Uri to check.
+     * @return Whether the Uri authority is DownloadsProvider.
+     */
+    public static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri
+                .getAuthority());
+    }
+
+    /**
+     * @param uri
+     *            The Uri to check.
+     * @return Whether the Uri authority is MediaProvider.
+     */
+    public static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri
+                .getAuthority());
+    }
+
+    /**
+     * @param uri
+     *            The Uri to check.
+     * @return Whether the Uri authority is Google Photos.
+     */
+    public static boolean isGooglePhotosUri(Uri uri) {
+        return "com.google.android.apps.photos.content".equals(uri
+                .getAuthority());
     }
 
 
